a98be4df8c81ba3831e87a5df8b453e01591e109
Varun Sudrik <varunsudrik99@gmail.com>
2023-10-15 20:01:53 +0530

    added ws

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 const path = require("path");
 const app = express();
 const server = createServer(app);
 app.use(express.static(path.resolve("./public")));
+port = 4500;
 
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("message", msg);
+    console.log("message: " + msg);
+  });
+});
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
 });
 
 app.get("/dodo", (req, res) => {
